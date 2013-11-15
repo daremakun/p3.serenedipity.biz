4b9dd18f9c8a640791a14c166e832e551a0e9713
DARE MAKUN <dremak11@gmail.com>
2013-11-15 13:01:00 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<link rel="stylesheet" href="" type="text/css">
+		
+	</head>
+	
+	<body>
+	
+		Hello World!
+		
+		<script>
+			alert('Hello World');
+		</script>
+		
+	</body>
+	
+</html>	
