e1c20eb8fcc59ca2257e2616d93d8603a96200b2
DARE MAKUN <dremak11@gmail.com>
2013-11-22 12:12:41 -0500

    js file added

---
+++ js/learn.js
@@ -0,0 +1,35 @@
+// When the document loads, initialize the button.
+$(
+function(){
+var jButton = $( "button" );
+ 
+// First, hide all the list items.
+$( "li" ).hide();
+ 
+jButton.data(
+"config",
+{
+Index: 0,
+Collection: $( "li" )
+}
+);
+ 
+jButton.click(
+function( objEvent ){
+var jThis = $( this );
+ 
+var objConfig = jThis.data( "config" );
+
+if (objConfig.Index < objConfig.Collection.length){
+ 
+$( objConfig.Collection[ objConfig.Index++ ] ).slideDown();
+ 
+}
+ 
+objEvent.preventDefault();
+return( false );
+}
+);
+}
+);
+ 
