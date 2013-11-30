287960d739f47eecad365fd86a36b7a0d2af45ba
DARE MAKUN <dremak11@gmail.com>
2013-11-30 15:12:20 -0500

    updated index

--- js/test.js
+++ js/test.js
@@ -1,8 +1,27 @@
-$('.button').on('click', function(){
-    $('.content').find('div').hide();
-    var getMessageDiv = '#'+$(this).attr('id')+"Div";
-    if ($(getMessageDiv).is(':visible'))
-    $(getMessageDiv).fadeOut();
-     else 
-    $(getMessageDiv).fadeIn();
-})
+counters = new Array();
+			
+			for (i=0;i<26;i++) {
+				counters[i] = 1;
+			}
+		
+			$(document).ready(function() {
+				$("li").css("display","none");
+			});
+			
+			$("button").click(function() {
+				letter = parseInt(this.getAttribute("alpha-id"));
+				hideAllWordsExcept(letter);
+				document.getElementById(letter).childNodes[counters[letter]].style.display = "block";
+				counters[letter] = parseInt(counters[letter]) + 2;
+			});
+			
+			function hideAllWordsExcept(letter) {
+			lis = document.getElementsByTagName("li");
+			for (i=0;i<lis.length;i++) {
+				if(!(lis[i].className == letter)) {
+					lis[i].style.display = "none";
+					counters[lis[1].className] = 1;
+					}
+				}
+			}
+			
